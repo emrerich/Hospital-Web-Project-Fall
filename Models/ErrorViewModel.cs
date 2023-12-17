7ca5c3e46eb9c486c22b951a84149724afadc989
emrerich <emre.zengin5@ogr.sakarya.edu.tr>
2023-12-17 20:50:15 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hospital_Web_Project_Fall.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
