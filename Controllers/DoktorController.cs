8cbf773af7893791a543920d0e265699f66bb738
emrerich <emre.zengin5@ogr.sakarya.edu.tr>
2023-12-21 23:37:09 +0300

    commited

---
+++ Controllers/DoktorController.cs
@@ -0,0 +1,13 @@
+﻿using Hospital_Web_Project_Fall.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Hospital_Web_Project_Fall.Controllers
+
+public class DoktorController
+{
+	public DoktorController()
+	{ }
+
+	}
+}
---
+++ Models/DoktorViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Hospital_Web_Project_Fall.Models
+{
+    public class DoktorViewModel
+    {
+        public string? DoktorAdi { get; set; }
+        public string? DoktorSoyadi { get; set; }
+        public string? DoktorBrans { get; set; }
+        public string? DoktorTelefon { get; set; }
+        public string? DoktorMail { get; set; }
+        public string? DoktorSifre { get; set; }
+    }
+}
